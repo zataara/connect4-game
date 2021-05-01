46cf14f7dbb0490b5d96e8d98e2b62e5e4b232f4
zataara <bryantmac@gmail.com>
2021-05-01 16:39:48 -0700

    applies setTimeout to alert function to game piece drops before alert

--- connect4.js
+++ connect4.js
@@ -81,7 +81,9 @@ function placeInTable(y, x) {
 
 function endGame(msg) {
   // TODO: pop up alert message
-  setTimeout(alert(msg), 4000)
+  setTimeout(function() {
+    alert(msg);
+   }, 9);
 }
 
 /** handleClick: handle click of column top to play piece */
